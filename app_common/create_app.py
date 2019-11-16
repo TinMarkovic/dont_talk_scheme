d4490398637e0941f77989fe82cd6d42c8a3bea0
Tin <tinmarkoviccs@gmail.com>
2019-11-16 08:35:14 +0200

    Convert bird_keepers to OpenAPI3

--- app_common/create_app.py
+++ app_common/create_app.py
@@ -1,26 +1,12 @@
-from os import path
-from importlib import import_module
-
 from connexion.apps.flask_app import FlaskApp
 from connexion.resolver import RestyResolver
-
-from app_common.schema_loader import prepare_schema
 
 
 def create_connexion_app(package_name: str) -> FlaskApp:
     connexion_app = FlaskApp(package_name)
 
-    package = import_module(package_name)
-    base_package = import_module("avian_schema")
-    connexion_app.openapi_schema = prepare_schema(
-        [
-            "{}/schema".format(path.dirname(path.realpath(package.__file__))),
-            "{}/schema".format(path.dirname(path.realpath(base_package.__file__))),
-        ]
-    )
-
     connexion_app.add_api(
-        connexion_app.openapi_schema,
+        'schema/openapi.yaml',
         validate_responses=True,
         strict_validation=True,
         resolver=RestyResolver("{}.handlers".format(package_name)),
--- bird_watchers/app.py
+++ bird_watchers/app.py
@@ -1,4 +1,32 @@
-from app_common.create_app import create_connexion_app
+from os import path
+from importlib import import_module
+
+from connexion.apps.flask_app import FlaskApp
+from connexion.resolver import RestyResolver
+
+from app_common.schema_loader import prepare_schema
+
+
+def create_connexion_app(package_name: str) -> FlaskApp:
+    connexion_app = FlaskApp(package_name)
+
+    package = import_module(package_name)
+    base_package = import_module("avian_schema")
+    connexion_app.openapi_schema = prepare_schema(
+        [
+            "{}/schema".format(path.dirname(path.realpath(package.__file__))),
+            "{}/schema".format(path.dirname(path.realpath(base_package.__file__))),
+        ]
+    )
+
+    connexion_app.add_api(
+        connexion_app.openapi_schema,
+        validate_responses=True,
+        strict_validation=True,
+        resolver=RestyResolver("{}.handlers".format(package_name)),
+    )
+    return connexion_app
+
 
 connexion_app = create_connexion_app("bird_watchers")
 connexion_app.run(port=8080)
